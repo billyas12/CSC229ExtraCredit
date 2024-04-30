6e768c1f8a897db89b693557dedf0cd5f526cf0c
billyas12 <145586511+billyas12@users.noreply.github.com>
2024-04-30 15:16:00 -0400

    Problem01

--- src/AnagramSorter.java
+++ src/AnagramSorter.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class AnagramSorter {
+import  java.util.*;
+public class AnagramSorter {
+    public static List<List<String>> sortAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            map.get(sortedStr).add(str);
+        }
+        List<List<String>> result = new ArrayList<>();
+        for (List<String> group : map.values()) {
+            result.add(group);
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat", "tab", "cat"};
+        List<List<String>> sortedAnagrams = sortAnagrams(strs);
+        for (List<String> group : sortedAnagrams) {
+            System.out.println(group);
+        }
+    }
 }
