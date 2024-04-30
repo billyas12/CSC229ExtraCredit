90aa0dad688db51fcd3a1a9879548a5b98322a76
billyas12 <145586511+billyas12@users.noreply.github.com>
2024-04-30 15:21:02 -0400

    Problem02

---
+++ src/MergeSortedArrays.java
@@ -0,0 +1,29 @@
+public class MergeSortedArrays {
+    public static void merge(int[] A, int m, int[] B, int n) {
+        int indexA = m - 1;
+        int indexB = n - 1;
+        int mergedIndex = m + n - 1;
+
+        while (indexA >= 0 && indexB >= 0) {
+            if (A[indexA] > B[indexB]) {
+                A[mergedIndex--] = A[indexA--];
+            } else {
+                A[mergedIndex--] = B[indexB--];
+            }
+        }
+        while (indexB >= 0) {
+            A[mergedIndex--] = B[indexB--];
+        }
+    }
+    public static void main(String[] args) {
+        int[] A = {1, 3, 5, 0, 0, 0};
+        int[] B = {2, 4, 6};
+        int m = 3;
+        int n = 3;
+        merge(A, m, B, n);
+
+        for (int num : A) {
+            System.out.print(num + ", ");
+        }
+    }
+}
