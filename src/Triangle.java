2ff4bd7617abb12e2506899920b18f1bf3ae27ca
billyas12 <145586511+billyas12@users.noreply.github.com>
2024-04-30 15:32:58 -0400

    Problem03

---
+++ src/Triangle.java
@@ -0,0 +1,26 @@
+public class Triangle {
+    public static long countNonDivisible(int numRows) {
+        long[][] triang = new long[numRows][numRows];
+        long count = 0;
+
+        for (int i = 0; i < numRows; i++) {
+            triang[i][0] = 1;
+            triang[i][i] = 1;
+
+            for (int j = 1; j < i; j++) {
+                triang[i][j] = triang[i - 1][j - 1] + triang[i - 1][j];
+                if (triang[i][j] % 7 != 0) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int numRows = 1000000000;
+        long result = countNonDivisible(numRows);
+        System.out.println("Number of entries that are not divisible by 7: " + result);
+    }
+}
